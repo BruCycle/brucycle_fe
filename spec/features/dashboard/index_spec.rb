cee7b931f323cc82fcdb7b23fff5277704ac6f98
Sean Culliton <smculliton@gmail.com>
2023-01-05 11:17:33 -0700

    Setup user factory, write tests for OAuth and user dashboard

---
+++ spec/features/dashboard/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'the User Dashboard page' do 
+#   As a user,
+#   when I visit my dashboard page,
+#   I want to see my Brübank with a button to delete a beer,
+#   I want to see my Lifetime Stats, including Gas Money Saved, Calories Burned, Beers Consumed, and Distance Biked
+
+  describe 'page has attributes' do 
+    it 'has lifetime stats and athlete data' do 
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/dashboard'
+
+      expect(page).to have_content("Welcome, #{user.firstname}")
+      expect(page).to have_content('Lifetime Stats')
+      expect(page).to have_content('Gas Money Saved')
+      expect(page).to have_content('Calories Burned')
+      expect(page).to have_content('Beers Consumed')
+      expect(page).to have_content('Distance Biked')
+
+      expect(page).to have_button('I Drank a Beer')
+      expect(page).to have_content('Brübank')
+    end
+  end
+
+
+  # As a user,
+  # when I visit my dashboard page,
+  # I want to see a button that takes me to my activities,
+  # so that I can see all my activities. When I click this button, I am directed to my activities page. 
+end
