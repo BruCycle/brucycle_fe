7ffac8b3ce8e18b7b0eb52a4cd4a7446215b562c
Sean Culliton <smculliton@gmail.com>
2023-01-05 16:30:45 -0700

    Remove commented out code

--- app/services/brucycle_service.rb
+++ app/services/brucycle_service.rb
@@ -8,16 +8,3 @@ class BrucycleService
     Faraday.new(url: 'http://localhost:3000/')
   end
 end
-
-# {:data=>
-#   {:id=>"62",
-#    :type=>"user",
-#    :attributes=>
-#     {:username=>"Kay Mart",
-#      :beers_drunk=>14,
-#      :brubank=>866.04,
-#      :tot_gas_money_saved=>7.5,
-#      :tot_calories_burned=>7.5,
-#      :tot_miles_biked=>0.00375}}}
-
-# api/v1/user?uid=STRAVA_UID
