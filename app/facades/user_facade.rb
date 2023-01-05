040721bff045dc438bc07920d6c21d6ff82c48ef
Sean Culliton <smculliton@gmail.com>
2023-01-05 15:00:16 -0700

    Remove prys

--- app/facades/user_facade.rb
+++ app/facades/user_facade.rb
@@ -3,7 +3,7 @@
 class UserFacade
   def self.user(strava_id)
     user_data = BrucycleService.get_user(strava_id)[:data][:attributes]
-    require 'pry'; binding.pry
+
     UserStats.new(user_data)
   end
 end
