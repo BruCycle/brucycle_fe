4bedf4fd58af6b98063dfe95a64df7ff4bdd5e1d
Sean Culliton <smculliton@gmail.com>
2023-01-06 13:51:55 -0700

    Finish refactoring views

--- app/services/brucycle_service.rb
+++ app/services/brucycle_service.rb
@@ -6,14 +6,14 @@ class BrucycleService
     end
     JSON.parse(response.body, symbolize_names: true)
   end
- 
-  def self.conn
-    Faraday.new(url: 'http://localhost:3000/')
-  end
 
   def self.drink_beer(strava_id)
     conn.patch("api/v1/user?drank=beer") do |req|
       req.headers['STRAVA_UID'] = strava_id.to_s
     end
+  end
+ 
+  def self.conn
+    Faraday.new(url: 'http://localhost:3000/')
   end
 end
