509aed31bafab355db046fda79c201cd2910cd87
Sean Culliton <smculliton@gmail.com>
2023-01-07 17:41:04 -0700

    Add button to return to dashboard from activities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     client_secret = ENV['strava_client_secret']
     code = params[:code]
 
-    conn = Faraday.new(url: 'https://www.strava.com', headers: {'Accept': 'application/json'})
+    conn = Faraday.new(url: 'https://www.strava.com', headers: { 'Accept': 'application/json' })
 
     response = conn.post('/api/v3/oauth/token') do |req|
       req.params['code'] = code
@@ -12,11 +12,16 @@ class SessionsController < ApplicationController
       req.params['client_secret'] = client_secret
       req.params['grant_type'] = 'authorization_code'
     end
-    
+
     data = JSON.parse(response.body, symbolize_names: true)
 
-    user = User.find_or_create_by(strava_id:data[:athlete][:id])
-    user.update(username:data[:athlete][:username], firstname:data[:athlete][:firstname], lastname:data[:athlete][:lastname], photo_url:data[:athlete][:profile])
+    user = User.find_or_create_by(strava_id: data[:athlete][:id])
+    user.update(photo_url: data[:athlete][:profile],
+                firstname: data[:athlete][:firstname],
+                lastname: data[:athlete][:lastname])
+
+    user.update(username: data[:athlete][:username]) if user.username.nil?
+
     session[:user_id] = user.id
     session[:token] = data[:access_token]
     redirect_to '/dashboard'
