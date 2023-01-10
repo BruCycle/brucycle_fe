b58805bf42a2fa41fb7451be72065995c25d4c57
Naomi Yocum <naomiyocum24@gmail.com>
2023-01-10 12:02:20 -0700

    add stubbing for activities request

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -21,10 +21,14 @@ RSpec.describe 'the Login page' do
   context 'user already logged in' do 
     before(:each) do 
       @json = File.read('spec/fixtures/user_data.json')
+      @json2 = File.read('spec/fixtures/user_activities.json')
       @user = create(:user)
 
       stub_request(:get, 'http://localhost:3000/api/v1/user?')
         .to_return(status: 200, body: @json)
+
+      stub_request(:get, 'http://localhost:3000/api/v1/activities?')
+        .to_return(status: 200, body: @json2)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user)
         .and_return(@user)
