6756d1baac5b4346ac4cdd1258cc08b50537d641
Sean Culliton <smculliton@gmail.com>
2023-01-10 14:54:11 -0700

    Move email password to application.yml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: 'brucycle.cares',
-    password: 'obhlqoeohgehpazx',
+    password: ENV['email_password'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
