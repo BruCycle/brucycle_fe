99fc8414c23497b81fe7b18192ea4cfbe7bbe368
Sean Culliton <smculliton@gmail.com>
2023-01-11 11:58:26 -0700

    Refactor Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,14 @@
 class DashboardController < ApplicationController
   before_action :check_for_user
+  before_action :first_time_login, only: :index
+  before_action :check_inebriation, only: :update
 
   def index
-    UserFacade.user(current_user.strava_uid, session[:token])
     UserFacade.get_activities(current_user.strava_uid, session[:token])
     @user_stats = UserFacade.user(current_user.strava_uid, session[:token])
   end
 
   def update
-    if current_user.inebriated?
-      SafetyMailer.with(user: current_user).safety_email.deliver_now if current_user.email
-      flash[:alert] = 'Our records indicate you might be inebriated, please take an Uber home'
-    end
-
     UserFacade.drink_beer(current_user.strava_uid)
     redirect_to '/dashboard'
   end
@@ -21,5 +17,16 @@ class DashboardController < ApplicationController
 
   def check_for_user
     redirect_to '/' unless current_user
+  end
+
+  def check_inebriation
+    if current_user.inebriated?
+      SafetyMailer.with(user: current_user).safety_email.deliver_now if current_user.email
+      flash[:alert] = 'Our records indicate you might be inebriated, please take an Uber home'
+    end
+  end
+
+  def first_time_login
+    UserFacade.user(current_user.strava_uid, session[:token]) if current_user.created_at > DateTime.now - 1.0/24
   end
 end
